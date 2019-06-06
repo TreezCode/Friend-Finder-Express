b7c53ec38c1b16fb06bf77316648cb044325c4ae
TreezCode <TreezCode@users.noreply.github.com>
2019-06-06 14:17:11 -0600

    add perm app data

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+
+// Create an array of objects to store app data
+var friendsArr = [
+    {
+        name:"Jenna Javascript",
+        photo: "https://gocoderz.com/wp-content/uploads/2018/04/hacking-2275592_1280.jpg",
+        scores: [
+            "2",
+            "4",
+            "1",
+            "1",
+            "5",
+            "3",
+            "5",
+            "3",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name:"Hannah Hypertext",
+        photo: "https://cdn-images-1.medium.com/max/724/1*cSh3-kLtiMBU88s2_CSw1w.png",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "4",
+            "3",
+            "4",
+            "5",
+            "3",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name:"Cody Cascade",
+        photo: "https://tomatomeng.files.wordpress.com/2014/01/coding-standards.jpg",
+        scores: [
+            "3",
+            "4",
+            "5",
+            "5",
+            "2",
+            "3",
+            "4",
+            "5",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name:"Scott Styles",
+        photo: "https://qpower.ca/images/approach_norima_computer_guy.jpg",
+        scores: [
+            "3",
+            "1",
+            "3",
+            "2",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name:"Amanda Algorithm",
+        photo: "https://gocoderz.com/wp-content/uploads/2018/04/hacking-2275592_1280.jpg",
+        scores: [
+            "5",
+            "5",
+            "3",
+            "4",
+            "5",
+            "1",
+            "4",
+            "5",
+            "5",
+            "5"
+        ]
+    },   
+];
+
+module.exports = friendsArr;
