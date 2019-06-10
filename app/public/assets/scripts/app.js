9802ba1448507d92fcdce10a956e45f474e45925
TreezCode <TreezCode@users.noreply.github.com>
2019-06-10 15:15:16 -0600

    user input validation

--- app/public/assets/scripts/app.js
+++ app/public/assets/scripts/app.js
@@ -3,7 +3,16 @@
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
-    // if (valid) {
+    let valid = true;
+
+    // User input validation
+    if ($("#name-input").val() === "" || $("#photo-input").val() === "") {
+        valid = false;
+    } else if ($("#q1").val() === "" || $("#q2").val() === "" || $("#q3").val() === "" || $("#q4").val() === "" || $("#q5").val() === "" || $("#q6").val() === "" ||$("#q7").val() === "" || $("#q8").val() === "" || $("#q9").val() === "" || $("#q10").val() === "") {       
+        valid = false;
+    }
+
+    if (valid === true) {
 
         // Cap user input
         let userInput = {
@@ -27,16 +36,17 @@ $("#submit").on("click", function(event) {
     
         // Ajax call to add new user to friends list
         $.post("/api/friends", userInput, function(data) {
+
+            // Testing / Debug
             console.log(data);
             
-    
             $("#match-name").text(data.name);
             $("#match-photo").attr("src", data.photo);
             $("#match-modal").modal("toggle");
 
         });
-    // } else {
-    //     // Alert if survey in not complete
-    //     alert("Incomplete Survey!")
-    // }
+    } else {
+        // Alert if survey in not complete
+        alert("Incomplete Survey!")
+    }
 });
