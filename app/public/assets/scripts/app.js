a624d1c2c9f0e333623acdc4b547629f7779dbed
TreezCode <TreezCode@users.noreply.github.com>
2019-06-14 10:37:14 -0600

    app complete

--- app/public/assets/scripts/app.js
+++ app/public/assets/scripts/app.js
@@ -19,16 +19,16 @@ $("#submit").on("click", function(event) {
             name : $("#name-input").val().trim(),
             photo : $("#photo-input").val().trim(),
             scores : [
-                $("#q1").val().trim(),
-                $("#q2").val().trim(),
-                $("#q3").val().trim(),
-                $("#q4").val().trim(),
-                $("#q5").val().trim(),
-                $("#q6").val().trim(),
-                $("#q7").val().trim(),
-                $("#q8").val().trim(),
-                $("#q9").val().trim(),
-                $("#q10").val().trim(),
+                $("#q1").val(),
+                $("#q2").val(),
+                $("#q3").val(),
+                $("#q4").val(),
+                $("#q5").val(),
+                $("#q6").val(),
+                $("#q7").val(),
+                $("#q8").val(),
+                $("#q9").val(),
+                $("#q10").val(),
             ]
         };
     
